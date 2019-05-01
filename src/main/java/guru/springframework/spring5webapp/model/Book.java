cdf8e71c5b17018f19eb1f2ae337258af54d64d5
Brandon Bordeaux <bbordeaux12@gmail.com>
2019-05-01 15:18:56 -0400

    Implemented JPA Author and Book entities

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -7,14 +7,22 @@ import java.util.Set;
 /**
  * Created by jt on 5/16/17.
  */
-
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String isbn;
     private String publisher;
 
+    // ManyToMany generated to join tables between the Book and Author classes.
+    // e.g. Book_Authors and Author_Books
+    // To correct to a proper schema, the @JoinTable annotation is used below.
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
     public Book() {
